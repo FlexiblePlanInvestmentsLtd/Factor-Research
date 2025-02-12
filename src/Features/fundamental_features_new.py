325a6047d3c5af60decdfb73782809a3cb1e017a
RajdeepDas <rajdeep.ind24@gmail.com>
2025-02-12 00:18:57 -0600

    Implemented technical and fundamental merging

--- src/Features/fundamental_features_new.py
+++ src/Features/fundamental_features_new.py
@@ -1,6 +1,7 @@
 #type: ignore
 import pandas as pd
 import sqlalchemy
+import yfinance as yf
 
 class BuildFeatures:
     def __init__(self, data, ticker):
@@ -10,16 +11,19 @@ class BuildFeatures:
 
 
     def build_master_data(self):
-        import yfinance as yf
+        # import yfinance as yf
         price = yf.Ticker(self.ticker)
         self.stock = price.history(period="max", interval="1d")
+        self.stock.index = self.stock.index.tz_convert('US/Eastern')
         for i,j in self.data.items():
             self.data[i] = j[j['ticker'] == self.ticker]
+            if 'per_type' in j.columns:
+                self.data[i] = self.data[i][self.data[i]['per_type']=='Q']
 
 
     def process_financial_statements(self):
         dates = ['filing_date']
-        indicator_features=["total revenue","ebitda","eps","Dividend Payout Ratio","total_current_assets"]
+        indicator_features=["total revenue","ebitda","eps","Dividend Payout Ratio","total_current_assets"," "]
         ratio_features = self.data['zacks_fr'].columns.tolist()
 
 
@@ -32,12 +36,32 @@ class BuildFeatures:
         self.fundamental_features.loc[:, "total_current_assets"] = self.data['zacks_fc'].loc[:, "tot_curr_asset"].values
 
         ### Dates ###
-        self.fundamental_features.loc[:, "filing_date"] = self.data['zacks_fc'].loc[:, "filing_date"].values
+        self.fundamental_features.loc[:, "filing_date"] = self.data['zacks_fc'].loc[:, "filing_date"]
 
         ### Ratios ###
         self.fundamental_features[ratio_features] = self.data['zacks_fr'][ratio_features]
-        self.fundamental_features = self.fundamental_features.merge(self.stock["Close"], left_index=True, right_index=True)
+        self.fundamental_features.set_index('filing_date', inplace=True)
+        self.fundamental_features = self.fundamental_features[~self.fundamental_features.index.duplicated(keep='last')]
+        # self.fundamental_features = pd.merge_asof(left=self.fundamental_features, right=self.stock['Close'], left_index=True, right_index=True)
 
+    def extend_monthly(self):
+        self.fundamental_features.index = self.fundamental_features.index.to_period('M').to_timestamp()
+        all_months = pd.date_range(start=self.fundamental_features.index.min(), end=self.fundamental_features.index.max(), freq='MS')
+
+        # Identify missing months
+        existing_months = self.fundamental_features.index.to_period('M')
+        missing_months = [date for date in all_months if date.to_period('M') not in existing_months]
+
+        # Add missing months to the dataset with NaN values for other columns
+        missing_data = pd.DataFrame({
+            'filing_date': missing_months
+        }).set_index('filing_date')
+
+        # Combine the original and missing data, then sort by index
+        self.fundamental_features = pd.concat([self.fundamental_features, missing_data]).sort_index()
+        self.fundamental_features.index = self.fundamental_features.index.tz_localize('UTC').tz_convert('US/Eastern')
+        self.fundamental_features.ffill(inplace=True)
+        
 
     def build_growth_rates(self, lookbacks):
         growth_features=[]
